ad59edf2d411ce44086b2be82a06b765ab3a6f0a
Abha Gupta <agupta3@walmart.com>
2020-02-05 15:27:31 -0800

    testing CI pr

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import Button from './components/button/button';
+import User from './components/connectedComponents/user/user';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+          <h1 className="App_title">Test application in React</h1>
+        </header>
+        <p className="App_intro">
+        Visual Regression Testing is pretty awesome!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <Button label ="I am button component rendered for demo"/>
+        <User />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/reducers/reducer-user.js
@@ -0,0 +1,25 @@
+export default function() {
+    return [
+        {
+            id: 1,
+            first: "Bucky",
+            last: "Roberts",
+            age: 25,
+            description: "Bucky is a great developer",
+        },
+        {
+            id: 2,
+            first: "Gordon",
+            last: "Ramsay",
+            age: 52,
+            description: "Gordon is a great chef",
+        },
+        {
+            id: 3,
+            first: "William",
+            last: "Penn",
+            age: 43,
+            description: "William is a great dancer",
+        }
+    ]
+}
