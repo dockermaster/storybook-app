dff1e45e62df0d982ea8769f07f220a7a781c55b
Abha Gupta <agupta3@walmart.com>
2020-02-06 11:07:45 -0800

    wait for added

--- tests/percy/homepage.js
+++ tests/percy/homepage.js
@@ -1,7 +1,8 @@
 const PercyScript = require('@percy/script');
 
 PercyScript.run(async (page, percySnapshot) => {
-  await page.goto('http://192.168.80.3:3000/');
+  await page.goto('http://localhost:3000/');
   // ensure the page has loaded before capturing a snapshot
+  await page.waitFor(".App_title");
   await percySnapshot('homepage');
 });
