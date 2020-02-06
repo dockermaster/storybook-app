e609615b95dc6ec1864d2d37030b117773e5de2f
Abha Gupta <agupta3@walmart.com>
2020-02-06 10:27:14 -0800

    fix port

--- tests/percy/homepage.js
+++ tests/percy/homepage.js
@@ -1,7 +1,7 @@
 const PercyScript = require('@percy/script');
 
 PercyScript.run(async (page, percySnapshot) => {
-  await page.goto('http://localhost:3001/');
+  await page.goto('http://localhost:3000/');
   // ensure the page has loaded before capturing a snapshot
   await percySnapshot('homepage');
 });
