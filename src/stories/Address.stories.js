2c845347cc65eb49f0674fa33ecb6a6d014444fe
Abha Gupta <agupta3@walmart.com>
2020-02-07 11:33:03 -0800

    adding some stories

---
+++ src/stories/Address.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Address from '../components/address/address';
+
+export default {
+    title: 'Address',
+    component: 'Address'
+}
+
+export const MyAddress = () => <Address />
---
+++ src/stories/Button.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button  from '../components/button/button';
+
+export default {
+  title: 'Button',
+  component: Button,
+};
+
+export const MyButton = () => <Button label="My Button"></Button>;
+
