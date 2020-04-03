e8d48be5226565dfb8ac7807334b8f085c620f05
Abha Gupta <agupta3@walmart.com>
2020-04-03 08:49:29 -0700

    testing options for percySnapshot

--- tests/percy/homepage.js
+++ tests/percy/homepage.js
@@ -4,5 +4,5 @@ PercyScript.run(async (page, percySnapshot) => {
   await page.goto('http://localhost:3000/');
   // ensure the page has loaded before capturing a snapshot
   await page.waitFor(".App_title");
-  await percySnapshot('homepage');
+  await percySnapshot('homepage', { widths: [350, 992, 1200] });
 });
